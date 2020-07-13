3e612092d82c8402114f5ac52b700c444d3bf1a9
Abdullah Mansoor <abdullahmansoor277@gmail.com>
2020-07-13 21:19:59 +0500

    Updates

--- precache-manifest.e502da9be89fc6e098695432a8703365.js
+++ precache-manifest.e502da9be89fc6e098695432a8703365.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6015a0083d7c3e349faf60d3189ca1b0",
+    "revision": "232845fda8ec72a1e50cb486833ffe44",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "8eed12a71b1a844816e4",
+    "revision": "60469628325fce4bf0c3",
     "url": "/portfolio/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "bf79e0fc48d59fd198f7",
-    "url": "/portfolio/static/js/2.4c736102.chunk.js"
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8eed12a71b1a844816e4",
-    "url": "/portfolio/static/js/main.10a1b4a7.chunk.js"
+    "revision": "60469628325fce4bf0c3",
+    "url": "/portfolio/static/js/main.24af9cb7.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
     "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "0a85b1329578729b2b98bb6004831a54",
+    "url": "/portfolio/static/media/logo.0a85b132.png"
   },
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
